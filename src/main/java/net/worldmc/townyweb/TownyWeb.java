47d70bd60e93fadd7cb45122e0c5aefde9d03377
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-22 14:36:44 +1000

    initialize webserver and serializer early

--- src/main/java/net/worldmc/townyweb/TownyWeb.java
+++ src/main/java/net/worldmc/townyweb/TownyWeb.java
@@ -1,12 +1,30 @@
 package net.worldmc.townyweb;
 
+import com.palmergames.bukkit.towny.TownyCommandAddonAPI;
+import net.worldmc.townyweb.towny.commands.NationBannerCommand;
+import net.worldmc.townyweb.towny.commands.TownBannerCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TownyWeb extends JavaPlugin {
+    private SerializerFactory serializerFactory;
+    private WebServer webServer;
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        WebServer webServer = new WebServer(this);
-        webServer.start();
+
+        serializerFactory = new SerializerFactory();
+        webServer = new WebServer(this);
+
+        TownyCommandAddonAPI.addSubCommand(TownyCommandAddonAPI.CommandType.TOWN_SET, "banner", new TownBannerCommand(this));
+        TownyCommandAddonAPI.addSubCommand(TownyCommandAddonAPI.CommandType.NATION_SET, "banner", new NationBannerCommand(this));
+    }
+
+    public SerializerFactory getSerializerFactory() {
+        return serializerFactory;
+    }
+
+    public WebServer getWebServer() {
+        return webServer;
     }
 }
