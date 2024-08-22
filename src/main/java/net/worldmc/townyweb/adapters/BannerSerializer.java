5cdf5ec07ebc6c4e09613bb0b1b242369b2c5dce
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-22 14:37:12 +1000

    banner serialization

---
+++ src/main/java/net/worldmc/townyweb/adapters/BannerSerializer.java
@@ -0,0 +1,32 @@
+package net.worldmc.townyweb.adapters;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.bukkit.block.Banner;
+import org.bukkit.block.banner.Pattern;
+import org.bukkit.inventory.meta.BannerMeta;
+
+import java.io.IOException;
+
+public class BannerSerializer extends StdSerializer<BannerMeta> {
+    public BannerSerializer() {
+        this(null);
+    }
+
+    public BannerSerializer(Class<BannerMeta> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(BannerMeta bannerMeta, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeStartArray();
+        for (Pattern pattern : bannerMeta.getPatterns()) {
+            gen.writeStartObject();
+            gen.writeStringField("pattern", pattern.getPattern().toString());
+            gen.writeStringField("color", pattern.getColor().toString());
+            gen.writeEndObject();
+        }
+        gen.writeEndArray();
+    }
+}
