3bf99e45ad2d5db2760656dead4a139f0fc008b4
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-08-23 03:39:30 +1000

    fix import

--- src/main/java/net/worldmc/townyweb/adapters/BannerSerializer.java
+++ src/main/java/net/worldmc/townyweb/adapters/BannerSerializer.java
@@ -3,7 +3,6 @@ package net.worldmc.townyweb.adapters;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import org.bukkit.block.Banner;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.inventory.meta.BannerMeta;
 
