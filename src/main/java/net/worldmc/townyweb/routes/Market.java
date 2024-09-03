43d503bedb471aff55249fa016c5035a5a4a5a67
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-09-04 04:20:23 +1000

    prepare for market route

---
+++ src/main/java/net/worldmc/townyweb/routes/Market.java
@@ -0,0 +1,9 @@
+package net.worldmc.townyweb.routes;
+
+import com.ghostchu.quickshop.api.QuickShopAPI;
+import io.javalin.http.Context;
+
+public class Market {
+    public void getShops(Context ctx) {
+    }
+}
